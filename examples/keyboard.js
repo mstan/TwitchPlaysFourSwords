17b8f42ef8d0bdc56d96bd725a6d284850c83fab
mstan <1379tech@gamil.com>
2020-11-30 23:21:28 -0800

    Clean up code. No functional change

--- examples/keyboard.js
+++ examples/keyboard.js
@@ -1,21 +1,20 @@
-var Keyboard = require('../lib/controls').Keyboard;
-var keyboard = new Keyboard();
+const Keyboard = require('../lib/controls').Keyboard;
+const keyboard = new Keyboard();
 
 // simulated valid keys
-var keys = [
+const keys = [
     '1', '2', //weapons
     'w', 's', 'a','d', 'space', // movement
     'q','e', 'r', 'shift', // abilities
     'x' // communication
     ]
 
-var randomKey = function() {
+const randomKey = function() {
     return keys[ Math.floor(Math.random() * keys.length) ]
 }
 
-var randomTime = function () {
-    var rand = Math.floor( Math.random() * 1000 * 5 );
-
+const randomTime = function () {
+    const rand = Math.floor( Math.random() * 1000 * 5 );
     return rand;
 }
 
